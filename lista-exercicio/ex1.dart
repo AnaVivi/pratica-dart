9f42d4a0b4fffe41d187b8158a5f9acfff203117
AnaVivi <avsl2003@hotmail.com>
2023-09-25 22:25:58 -0300

    wip adpaptando codigo p/ usar switch case

--- lista-exercicio/ex1.dart
+++ lista-exercicio/ex1.dart
@@ -3,7 +3,7 @@ void main(){
    var expectador = 0;
    var totalIdadeExcelente = 0;
    var quantidadeExcelente = 0;
-    var quantidadeRegular = 0; 
+   var quantidadeRegular = 0; 
    while(expectador <= 20){
     print("Pesquisa de satisfação da Cinépolis para o filme  “Senhor dos Anéis”");
     print("Para iniciar, por favor, digite a sua idade: ");
@@ -15,7 +15,22 @@ void main(){
     print("Qual a sua opinião do filme? Escolha:\n1 - Regular\n"+ 
     "2 - Bom\n" + "3 - Excelente\n");
     String? input = stdin.readLineSync();
-    print("Sua escolha é $input");
+    switch (input){
+      case "1":
+      print("Sua escolha é $input");
+      if (input != null) {
+         quantidadeRegular++;
+      if (quantidadeRegular > 0) { // Adicione esta parte para exibir a quantidade  de respostas "Regular"
+        print("Quantidade de pessoas que responderam 'Regular': $quantidadeRegular");
+      } else {
+        print("Ninguém respondeu 'Regular'.");
+      }
+    }
+    }
+
+    //Parei aqui: em refatorar o código. O adaptando p/ estrutura switch case.
+
+    //----------------------------------------------------------------
     if (input == '3') { // Verifica se a opinião é "Excelente"
         totalIdadeExcelente += idade; // Adiciona a idade à soma das idades Excelentes
         quantidadeExcelente++; // Incrementa a quantidade de respostas Excelentes
@@ -35,13 +50,9 @@ void main(){
     print("Ninguém respondeu 'Excelente'.");
   }
 
-  if (quantidadeRegular > 0) { // Adicione esta parte para exibir a quantidade de respostas "Regular"
-    print("Quantidade de pessoas que responderam 'Regular': $quantidadeRegular");
-  } else {
-    print("Ninguém respondeu 'Regular'.");
-  }
-
 }
+
+
 
 /*print("Resultado da pesquisa");
     if (input == "3") {
